9b1667808682b8df1c81bc3308c1b56ef5e54ea8
Evan Vigil-McClanahan <evan@heroku.com>
2015-05-20 20:51:37 -0700

    improve token capitalization performance
    rewrite it to more heavily uses bifs and to keep the intermediate state
    in an iolist rather than a binary.  for whatever nonsense beam reasons
    this is faster.  could have something to do with tiny binaries and
    refcounting but I can't even speculate.  This solution approaches the
    speed of just lowercasing the entire binary in a binary comprehension,
    so I don't imagine we can get much faster.

--- src/cowboyku_bstr.erl
+++ src/cowboyku_bstr.erl
@@ -33,15 +33,19 @@
 %% badly implemented clients.
 -spec capitalize_token(B) -> B when B::binary().
 capitalize_token(B) ->
-	capitalize_token(B, true, <<>>).
+    capitalize_token(B, undefined, []).
+
 capitalize_token(<<>>, _, Acc) ->
-	Acc;
-capitalize_token(<< $-, Rest/bits >>, _, Acc) ->
-	capitalize_token(Rest, true, << Acc/binary, $- >>);
-capitalize_token(<< C, Rest/bits >>, true, Acc) ->
-	capitalize_token(Rest, false, << Acc/binary, (char_to_upper(C)) >>);
-capitalize_token(<< C, Rest/bits >>, false, Acc) ->
-	capitalize_token(Rest, false, << Acc/binary, (char_to_lower(C)) >>).
+    iolist_to_binary(lists:reverse(Acc));
+capitalize_token(<<C, B/binary>>, undefined, Acc) ->
+    C1 = char_to_upper(C),
+    capitalize_token(B, C1, [C1|Acc]);
+capitalize_token(<<C, B/binary>>, $-, Acc) ->
+    C1 = char_to_upper(C),
+    capitalize_token(B, C1, [C1|Acc]);
+capitalize_token(<<C, B/binary>>, _, Acc) ->
+    C1 = char_to_lower(C),
+    capitalize_token(B, C1, [C1|Acc]).
 
 %% @doc Convert a binary string to lowercase.
 -spec to_lower(B) -> B when B::binary().
